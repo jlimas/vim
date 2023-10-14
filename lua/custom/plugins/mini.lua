83cb97f72556de91b7bf8eb2945141b80c3b3b3b
Jorge Limas <368854+jlimas@users.noreply.github.com>
2023-10-14 01:33:09 -0600

    Configured the mini plugins

---
+++ lua/custom/plugins/mini.lua
@@ -0,0 +1,9 @@
+return {
+  'echasnovski/mini.nvim',
+  version = false,
+  config = function()
+    require('mini.comment').setup {}
+    require('mini.surround').setup {}
+    require('mini.ai').setup {}
+  end,
+}
