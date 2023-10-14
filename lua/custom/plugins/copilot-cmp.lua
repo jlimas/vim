2890380c57e789427d66a200a319b7fcfdc4fc45
Jorge Limas <368854+jlimas@users.noreply.github.com>
2023-10-14 13:09:20 -0600

    Add toggle functionality for copilot

--- lua/custom/plugins/copilot-cmp.lua
+++ lua/custom/plugins/copilot-cmp.lua
@@ -1,6 +1,25 @@
 return {
   'zbirenbaum/copilot-cmp',
+  dependencies = {
+    'rcarriga/nvim-notify',
+  },
   config = function()
     require('copilot_cmp').setup()
+    local notify = require 'notify'
+
+    local enable = function()
+      vim.cmd ':Copilot enable'
+      notify('Ready', 'info', { title = 'Copilot' })
+    end
+
+    local disable = function()
+      vim.cmd ':Copilot disable'
+      notify('Disabled', 'warn', { title = 'Copilot' })
+    end
+
+    disable()
+
+    vim.keymap.set('n', '<leader>cu', enable, { desc = 'Enable Copilot' })
+    vim.keymap.set('n', '<leader>cd', disable, { desc = 'Disable Copilot' })
   end,
 }
