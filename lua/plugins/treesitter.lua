008c0ab5cab6632928f4d984c5be56a7420fc20d
Jorge Limas <368854+jlimas@users.noreply.github.com>
2023-10-19 13:23:11 -0600

    Fix issue with treesitter autoclose tags

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,21 +8,26 @@ return {
       'windwp/nvim-ts-autotag',
     },
     config = function()
-      -- import nvim-treesitter plugin
       local treesitter = require 'nvim-treesitter.configs'
 
-      -- configure treesitter
-      treesitter.setup { -- enable syntax highlighting
+      treesitter.setup {
         highlight = {
           enable = true,
         },
-        -- enable indentation
-        indent = { enable = true },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = {
+        indent = {
           enable = true,
         },
-        -- ensure these language parsers are installed
+        incremental_selection = {
+          enable = false,
+        },
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+        autotag = {
+          enable = true,
+          enable_close_on_slash = false,
+        },
         ensure_installed = {
           'json',
           'javascript',
@@ -40,20 +45,6 @@ return {
           'gitignore',
           'query',
           'go',
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<C-space>',
-            node_incremental = '<C-space>',
-            scope_incremental = false,
-            node_decremental = '<bs>',
-          },
-        },
-        -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
         },
       }
     end,
