1201573dd4993d987a8fe9e6acf1d13775255b52
Jorge Limas <368854+jlimas@users.noreply.github.com>
2023-10-25 19:21:43 -0600

    Disable the kind for the cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -11,34 +11,6 @@ return {
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
 
-    local kind_icons = {
-      Text = '',
-      Method = 'm',
-      Function = '',
-      Constructor = '',
-      Field = '',
-      Variable = '',
-      Class = '',
-      Interface = '',
-      Module = '',
-      Property = '',
-      Unit = '',
-      Value = '',
-      Enum = '',
-      Keyword = '',
-      Snippet = '',
-      Color = '',
-      File = '',
-      Reference = '',
-      Folder = '',
-      EnumMember = '',
-      Constant = '',
-      Struct = '',
-      Event = '',
-      Operator = '',
-      TypeParameter = '',
-    }
-
     cmp.setup {
       completion = {
         completeopt = 'menu,menuone,preview,noselect',
@@ -59,9 +31,6 @@ return {
       formatting = {
         fields = { 'kind', 'abbr', 'menu' },
         format = function(entry, vim_item)
-          -- Kind icons
-          vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
-          -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
           vim_item.menu = ({
             copilot = '[Copilot]',
             luasnip = 'LuaSnip',
