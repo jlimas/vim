7896d740241807ef91207500a1e56d14da26094c
Jorge Limas <368854+jlimas@users.noreply.github.com>
2023-11-14 23:31:55 -0700

    Disable the luasnips

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,16 +4,16 @@ return {
   build = 'make install_jsregexp',
   dependencies = {
     'rcarriga/nvim-notify',
+    'rafamadriz/friendly-snippets',
   },
   config = function()
     local ls = require 'luasnip'
 
-    require('luasnip.loaders.from_lua').load { paths = '~/.config/nvim/snippets/' }
+    -- Disabling the Lua snippets for now
+    -- require('luasnip.loaders.from_vscode').lazy_load {}
     require('luasnip').config.setup { store_selection_keys = '<A-p>' }
 
     vim.keymap.set('n', '<Leader><CR>', '<cmd>LuaSnipEdit<cr>', { silent = true, noremap = true })
-
-    vim.cmd [[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]]
 
     ls.config.set_config {
       history = true,
