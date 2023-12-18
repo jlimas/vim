dc3dc1f8b2f2424f1ff5054701fb376d50e84ca0
Jorge Limas <368854+jlimas@users.noreply.github.com>
2023-12-18 15:23:38 -0700

    Add command to copy the current file path

--- init.lua
+++ init.lua
@@ -176,3 +176,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.diagnostic.config {
   float = { border = 'rounded' },
 }
+
+vim.api.nvim_create_user_command('CopyFilePath', function()
+  local path = vim.fn.expand '%:p'
+  vim.fn.setreg('+', path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
