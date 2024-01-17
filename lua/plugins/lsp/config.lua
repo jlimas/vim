42d4f4b22391fb4da08baae24ca707209f78a2aa
Jorge Limas <368854+jlimas@users.noreply.github.com>
2024-01-17 14:53:03 -0700

    Display hover LSP in new buffer with leader K

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -8,6 +8,36 @@ return {
   config = function()
     require('neodev').setup {}
 
+    local hoverFunction = function()
+      local util = require 'vim.lsp.util'
+      vim.lsp.buf_request(0, 'textDocument/hover', util.make_position_params(), function(_, result, ctx, config)
+        config = config or {}
+        config.focus_id = ctx.method
+        if not (result and result.contents) then
+          -- return { 'No information available' }
+          return
+        end
+        local markdown_lines = util.convert_input_to_markdown_lines(result.contents)
+        -- trims beg and end of whole content
+        markdown_lines = util.trim_empty_lines(markdown_lines)
+        if vim.tbl_isempty(markdown_lines) then
+          return
+        end
+
+        -- print(dump(markdown_lines))
+        vim.api.nvim_command [[ new ]]
+        vim.api.nvim_buf_set_lines(0, 0, 1, false, markdown_lines)
+        vim.api.nvim_command [[ setlocal ft=markdown ]]
+        vim.api.nvim_command [[ nnoremap <buffer>q <C-W>c ]]
+        vim.api.nvim_command [[ setlocal buftype+=nofile ]]
+        vim.api.nvim_command [[ setlocal nobl ]]
+        vim.api.nvim_command [[ setlocal conceallevel=2 ]]
+        vim.api.nvim_command [[ setlocal concealcursor+=n ]]
+        vim.api.nvim_command [[ resize 5 ]]
+        -- vim.api.nvim_command [[ setlocal ft=lsp_markdown ]]
+      end)
+    end
+
     local lspconfig = require 'lspconfig'
 
     local util = require 'lspconfig.util'
@@ -66,6 +96,8 @@ return {
 
       opts.desc = 'Restart LSP'
       keymap.set('n', '<leader>rs', ':LspRestart<CR>', opts)
+
+      keymap.set('n', '<leader>k', hoverFunction, opts)
     end
 
     -- used to enable autocompletion (assign to every lsp server config)
