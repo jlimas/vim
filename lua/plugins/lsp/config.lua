9efa00d39965be8729682058fc5f6e8db4f0bfd7
Jorge Limas <368854+jlimas@users.noreply.github.com>
2024-01-18 12:30:14 -0700

    Configuration for Go PLS

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -148,6 +148,10 @@ return {
       settings = {
         gopls = {
           completeUnimported = true,
+          usePlaceholders = true,
+          analyses = {
+            uunusedparams = true,
+          },
         },
       },
     }
