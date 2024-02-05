b60b932f16de977b41b59a454b309a6dddf8767e
Jorge Limas <368854+jlimas@users.noreply.github.com>
2024-02-05 15:38:45 -0700

    Configure the coverge plugin

--- lua/plugins/coverage.lua
+++ lua/plugins/coverage.lua
@@ -1,4 +1,10 @@
 return {
   'andythigpen/nvim-coverage',
-  opts = {},
+  config = function()
+    local coverage = require 'coverage'
+    coverage.setup {}
+    vim.keymap.set('n', '<leader>ccj', '<cmd>Coverage<cr>', { desc = 'Coverage Show' })
+    vim.keymap.set('n', '<leader>ccs', '<cmd>CoverageSummary<cr>', { desc = 'Coverage Summary' })
+    vim.keymap.set('n', '<leader>cct', '<cmd>CoverageToggle<cr>', { desc = 'Coverage Toggle' })
+  end,
 }
