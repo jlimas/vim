53e9d455077b19fe329f685daffe230aec13ca85
Jorge Limas <368854+jlimas@users.noreply.github.com>
2024-02-09 16:19:57 -0700

    Command to restart eslint

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,8 +1,11 @@
 return {
   'mfussenegger/nvim-lint',
   config = function()
+    local notify = require 'notify'
+
     require('lint').linters_by_ft = {
       javascript = { 'eslint_d' },
+      typescript = { 'eslint_d' },
     }
 
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
@@ -10,5 +13,12 @@ return {
         require('lint').try_lint()
       end,
     })
+
+    -- Restart eslint_d
+    local restartEsLintD = function()
+      vim.cmd 'silent !eslint_d restart'
+      notify.notify('Restarted ESLint', 'info', { title = 'Linters' })
+    end
+    vim.keymap.set('n', '<leader>re', restartEsLintD, { desc = 'Restart ESLint_D' })
   end,
 }
