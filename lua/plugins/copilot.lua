46c6189f438e06a3cfc041316bee4987880035bf
Jorge Limas <368854+jlimas@users.noreply.github.com>
2024-02-10 19:32:21 -0700

    Keep track of copilot status

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,8 +1,20 @@
 return {
   'zbirenbaum/copilot.lua',
   cmd = 'Copilot',
-  event = 'InsertLeave',
+  event = 'InsertEnter',
   config = function()
+    local notify = require 'notify'
+    local enabled = true
+
+    vim.keymap.set('n', '=', function()
+      local command = enabled and ':Copilot enable' or ':Copilot disable'
+      local copilot_status = enabled and 'Enabled' or 'Disabled'
+      local level = enabled and 'info' or 'warn'
+      vim.cmd(command)
+      notify.notify(copilot_status, level, { title = 'Copilot' })
+      enabled = not enabled
+    end, { desc = 'Toggle Copilot' })
+
     require('copilot').setup {
       suggestion = {
         enabled = true,
