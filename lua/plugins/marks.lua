483f9b884837c42a6746245c3bcdbab07569b2c0
Jorge Limas <368854+jlimas@users.noreply.github.com>
2024-03-20 14:35:00 -0600

    replace grapple with marks

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -1,4 +1,19 @@
 return {
   'chentoast/marks.nvim',
   opts = {},
+  keys = {
+    { '<leader>mb', '<cmd>MarksListBuf<cr>', desc = '[M]arks [B]uffer' },
+    { '<leader>mg', '<cmd>MarksListGlobal<cr>', desc = '[M]arks [G]lobal' },
+    { '<leader>ma', '<cmd>MarksListAll<cr>', desc = '[M]arks [A]ll' },
+
+    { '<leader>m1', '<cmd>BookmarksList 1<cr>', desc = 'Bookmarks Group 1' },
+    { '<leader>m2', '<cmd>BookmarksList 2<cr>', desc = 'Bookmarks Group 2' },
+    { '<leader>m3', '<cmd>BookmarksList 3<cr>', desc = 'Bookmarks Group 3' },
+    { '<leader>m4', '<cmd>BookmarksList 4<cr>', desc = 'Bookmarks Group 4' },
+    { '<leader>m5', '<cmd>BookmarksList 5<cr>', desc = 'Bookmarks Group 5' },
+    { '<leader>m6', '<cmd>BookmarksList 6<cr>', desc = 'Bookmarks Group 6' },
+    { '<leader>m7', '<cmd>BookmarksList 7<cr>', desc = 'Bookmarks Group 7' },
+    { '<leader>m8', '<cmd>BookmarksList 8<cr>', desc = 'Bookmarks Group 8' },
+    { '<leader>m9', '<cmd>BookmarksList 9<cr>', desc = 'Bookmarks Group 9' },
+  },
 }
